8330d8303f0679d4d38bba7ce89364cac9b9ca3b
Diedrish Leonardo Zamuria Yanes <diedrishzamuria@gmail.com>
2020-12-06 08:25:00 -0600

    Update orden.php

--- Vistas/ordenes/orden.php
+++ Vistas/ordenes/orden.php
@@ -52,7 +52,7 @@
 
     <!-- NAVIGATION  -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Prefactura</a>
+        <a class="navbar-brand" href="#">Prefacturacion</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
